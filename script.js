87a5efb3dc1b3a744aea6d5dfd6bf8addb962457
VenBak <andre.brahin@gmail.com>
2022-11-30 19:09:19 -0500

    added files to repo

---
+++ script.js
@@ -0,0 +1,22 @@
+var start = document.getElementById("gamestarter");
+var hangman = document.getElementById("textcontent");
+var timer = document.getElementById("timer");
+words = 
+
+function Playgame(){
+    // Pick a random word out of an array or object
+
+
+    
+    
+    // start the timer
+
+
+
+}
+
+
+
+
+
+start.addEventListener("click",);
